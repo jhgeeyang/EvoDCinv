91cb7d5237656397b66dc31645817508ed576928
jhgeeyang <jihyunyang@mymail.mines.edu>
2019-01-17 20:47:44 -0700

    example

--- examples/output/viewPickle.py
+++ examples/output/viewPickle.py
@@ -2,5 +2,7 @@ import pickle
 
 # loaded data is a "layered-model" object
 a = pickle.load(open('test.pickle','rb'))
-a = pickle.load(open('test.pickle','rb'))
+b = pickle.load(open('run1.pickle','rb'))
 print(a.model)
+print(b.model)
+
